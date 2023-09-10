db92f5e1dff73ac4c5f6ccdc932a201b4c6ce31f
fdezbarroso <f.fdezbarroso@gmail.com>
2023-09-10 17:57:16 +0200

    Add and load font

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 
 template <typename T, std::size_t S>
-class LimitedStack : public std::stack<T>
+class limited_stack : public std::stack<T>
 {
 private:
     std::size_t max_size;
 
 public:
-    LimitedStack() : max_size(S) {}
+    limited_stack() : max_size(S) {}
 
     void push(const T &value)
     {
@@ -23,24 +23,49 @@ public:
     }
 };
 
-struct Chip8
-{
-    std::array<std::uint8_t, 16> registers{};
-    std::array<std::uint8_t, 4096> memory{};
-    std::uint16_t index_register{};
+void load_font();
 
-    std::uint16_t pc{};
-    LimitedStack<std::uint16_t, 16> stack{};
-    std::uint8_t sp{};
+std::array<std::uint8_t, 16> registers{};
+std::array<std::uint8_t, 4096> memory{};
+std::uint16_t index_register{};
 
-    std::uint8_t delay_timer{};
-    std::uint8_t sound_timer{};
+std::uint16_t pc{};
+limited_stack<std::uint16_t, 16> stack{};
+std::uint8_t sp{};
 
-    std::array<std::uint8_t, 16> keys{};
-    std::array<std::uint32_t, 64 * 32> display{};
+std::uint8_t delay_timer{};
+std::uint8_t sound_timer{};
+
+std::array<std::uint8_t, 16> keys{};
+std::array<std::uint32_t, 64 * 32> display{};
+
+std::array<uint8_t, 80> font{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-int main()
+main()
 {
+    load_font();
     return 0;
+}
+
+void load_font()
+{
+    for (int i{0x050}; i < 0x09F; i++)
+        memory.at(i) = font.at(i - 0x050);
 }
