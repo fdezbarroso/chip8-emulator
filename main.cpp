6374bcc6566d8e8f9e938c8a7811a4eb0efb3991
fdezbarroso <f.fdezbarroso@gmail.com>
2023-09-12 22:48:46 +0200

    Create simple SDL window

--- main.cpp
+++ main.cpp
@@ -34,6 +34,9 @@ void load_ROM(const char *rom_path);
 const char *ROM_LOCATION{"ROMs/logo.ch8"};
 const unsigned int START_ADDRESS{0x200};
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 320;
+
 const std::array<uint8_t, 80> font{
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -79,8 +82,40 @@ int main(int argc, char *argv[])
     {
     }
 
-    std::cout << SDL_Init(SDL_INIT_VIDEO) << std::endl;
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+    }
+    else
+    {
+        window = SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (window == NULL)
+        {
+            std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+            SDL_UpdateWindowSurface(window);
+            SDL_Event e;
+            bool quit = false;
+            while (quit == false)
+            {
+                while (SDL_PollEvent(&e))
+                {
+                    if (e.type == SDL_QUIT)
+                        quit = true;
+                }
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     std::cout << "Emulator terminated" << std::endl;
