4d998a5f698e78c46ed3c233ac3ea99577056565
fdezbarroso <f.fdezbarroso@gmail.com>
2023-09-14 19:41:43 +0200

    Implement instruction decodification

--- main.cpp
+++ main.cpp
@@ -31,7 +31,9 @@ public:
 // Loads the font into memory, starting at address 0x050 and finishing at 0x09F
 void load_font();
 // Loads the .ch8 ROM file's contents into memory when given a path to it
-bool load_ROM(const std::string rom_path);
+bool load_ROM(const std::string &rom_path);
+// Decodes the opcode's intruction and calls the corresponding execution function
+bool execute(const std::uint16_t &opcode);
 
 const std::string ROM_LOCATION{"ROMs/logo.ch8"};
 const unsigned int START_ADDRESS{0x200};
@@ -81,6 +83,7 @@ int main(int argc, char *argv[])
     load_font();
     if (!load_ROM(ROM_LOCATION))
     {
+        std::cerr << "Fatal error, execution aborted" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -121,6 +124,12 @@ int main(int argc, char *argv[])
                 opcode = memory.at(pc) << 8 | memory.at(pc + 1);
                 pc += 2;
 
+                if (!execute(opcode))
+                {
+                    std::cerr << "Fatal error, execution aborted" << std::endl;
+                    return EXIT_FAILURE;
+                }
+
                 SDL_UpdateWindowSurface(window);
             }
         }
@@ -141,7 +150,7 @@ void load_font()
         memory.at(i) = font.at(i - 0x050);
 }
 
-bool load_ROM(const std::string rom_path)
+bool load_ROM(const std::string &rom_path)
 {
     std::ifstream rom_file(rom_path, std::ios::binary);
 
@@ -170,5 +179,285 @@ bool load_ROM(const std::string rom_path)
     }
 
     rom_file.close();
+    return true;
+}
+
+bool execute(const std::uint16_t &opcode)
+{
+    // Extract hexadecimal nibbles
+    std::uint16_t n1{}, n2{}, n3{}, n4{};
+    n1 = (opcode >> 12) & 0xF;
+    n2 = (opcode >> 8) & 0xF;
+    n3 = (opcode >> 4) & 0xF;
+    n4 = opcode & 0xF;
+
+    // Decode instruction and call corresponding function
+    switch (n1)
+    {
+    // 00E0, 00EE
+    // 0NNN not implemented
+    case 0x0:
+        if (n2 != 0x0 || n3 != 0xE)
+        {
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+
+        switch (n4)
+        {
+        case 0x0:
+            std::cout << "CLS" << std::endl;
+            break;
+
+        case 0xE:
+            std::cout << "RET" << std::endl;
+            break;
+
+        default:
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+        break;
+
+    // 1NNN
+    case 0x1:
+        std::cout << "JP addr" << std::endl;
+        break;
+
+    // 2NNN
+    case 0x2:
+        std::cout << "CALL addr" << std::endl;
+        break;
+
+    // 3XKK
+    case 0x3:
+        std::cout << "SE Vx, byte" << std::endl;
+        break;
+
+    // 4XKK
+    case 0x4:
+        std::cout << "SNE Vx, byte" << std::endl;
+        break;
+
+    // 5XY0
+    case 0x5:
+        if (n4 != 0x0)
+        {
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+
+        std::cout << "SE Vx, Vy" << std::endl;
+        break;
+
+    // 6XKK
+    case 0x6:
+        std::cout << "LD Vx, byte" << std::endl;
+        break;
+
+    // 7XKK
+    case 0x7:
+        std::cout << "ADD Vx, byte" << std::endl;
+        break;
+
+    // 8XY0, 8XY1, 8XY2, 8XY3, 8XY4, 8XY5, 8XY6, 8XY7, 8XYE
+    case 0x8:
+        switch (n4)
+        {
+        case 0x0:
+            std::cout << "LD Vx, Vy" << std::endl;
+            break;
+
+        case 0x1:
+            std::cout << "OR Vx, Vy" << std::endl;
+            break;
+
+        case 0x2:
+            std::cout << "AND Vx, Vy" << std::endl;
+            break;
+
+        case 0x3:
+            std::cout << "XOR Vx, Vy" << std::endl;
+            break;
+
+        case 0x4:
+            std::cout << "ADD Vx, Vy" << std::endl;
+            break;
+
+        case 0x5:
+            std::cout << "SUB Vx, Vy" << std::endl;
+            break;
+
+        case 0x6:
+            std::cout << "SHR Vx {, Vy}" << std::endl;
+            break;
+
+        case 0x7:
+            std::cout << "SUBN Vx, Vy" << std::endl;
+            break;
+
+        case 0xE:
+            std::cout << "SHL Vx {, Vy}" << std::endl;
+            break;
+
+        default:
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+        break;
+
+    // 9XY0
+    case 0x9:
+        if (n4 != 0x0)
+        {
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+
+        std::cout << "SNE Vx, Vy" << std::endl;
+        break;
+
+    // ANNN
+    case 0xA:
+        std::cout << "LD I, addr" << std::endl;
+        break;
+
+    // BNNN
+    case 0xB:
+        std::cout << "JP V0, addr" << std::endl;
+        break;
+
+    // CXKK
+    case 0xC:
+        std::cout << "RND Vx, byte" << std::endl;
+        break;
+
+    // DXYN
+    case 0xD:
+        std::cout << "DRW Vx, Vy, nibble" << std::endl;
+        break;
+
+    // EX9E, EXA1
+    case 0xE:
+        switch (n3)
+        {
+        case 0x9:
+            if (n4 != 0xE)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "SKP Vx" << std::endl;
+            break;
+
+        case 0xA:
+            if (n4 != 0x1)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "SKNP Vx" << std::endl;
+            break;
+
+        default:
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+        break;
+
+    // Fx07, Fx0A, Fx15, Fx18, Fx1E, Fx29, Fx33, Fx55, Fx65
+    case 0xF:
+        switch (n3)
+        {
+        case 0x0:
+            switch (n4)
+            {
+            case 0x7:
+                std::cout << "LD Vx, DT" << std::endl;
+                break;
+
+            case 0xA:
+                std::cout << "LD Vx, K" << std::endl;
+                break;
+
+            default:
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+            break;
+
+        case 0x1:
+            switch (n4)
+            {
+            case 0x5:
+                std::cout << "LD DT, Vx" << std::endl;
+                break;
+
+            case 0x8:
+                std::cout << "LD ST, Vx" << std::endl;
+                break;
+
+            case 0xE:
+                std::cout << "ADD I, Vx" << std::endl;
+                break;
+
+            default:
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+            break;
+
+        case 0x2:
+            if (n4 != 0x9)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "LD F, Vx" << std::endl;
+            break;
+
+        case 0x3:
+            if (n4 != 0x3)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "LD B, Vx" << std::endl;
+            break;
+
+        case 0x5:
+            if (n4 != 0x5)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "LD [I], Vx" << std::endl;
+            break;
+
+        case 0x6:
+            if (n4 != 0x5)
+            {
+                std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+                return false;
+            }
+
+            std::cout << "LD Vx, [I]" << std::endl;
+            break;
+
+        default:
+            std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+            return false;
+        }
+        break;
+
+    default:
+        std::cerr << "Invalid instruction. Opcode: " << std::hex << opcode << std::endl;
+        return false;
+    }
     return true;
 }
